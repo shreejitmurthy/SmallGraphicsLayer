c8b2f0705cf8734c770f263f20b9a1248d3f40b1
Shreejit Murthy <botmorph17@gmail.com>
2025-08-16 01:14:40 +1000

    clear and render functions

--- include/SGL/SmallGraphicsLayer.hpp
+++ include/SGL/SmallGraphicsLayer.hpp
@@ -1,3 +1,16 @@
 #pragma once
 
-void HelloSGL();
+#include <sokol/sokol_gfx.h>
+#include <cstdint>
+
+typedef sg_color SGLColour;
+
+class SimpleGraphicsLayer {
+public:
+    void Init(/* SGLFlags flags, */ int width = 0, int height = 0);
+    void Clear(SGLColour clear_col = {0.15f, 0.15f, 0.2f, 1.0f});
+    void Render();
+private:
+    sg_pass_action pass_action;
+    sg_swapchain swapchain;
+};
--- src/SmallGraphicsLayer.cpp
+++ src/SmallGraphicsLayer.cpp
@@ -2,6 +2,54 @@
 
 #include <iostream>
 
+#if defined(WINDOW_SAPP)
+    #define SOKOL_GLUE_IMPL
+    #define SOKOL_LOG_IMPL
+    #include <sokol/sokol_glue.h>
+    #include <sokol/sokol_log.h>
+#endif
+
+#define SOKOL_GFX_IMPL
+#define SOKOL_GLCORE
+#include <sokol/sokol_gfx.h>
+
+#include "SGL/SmallGraphicsLayer.hpp"
+
 void HelloSGL() {
     std::cout << "Hello, SGL!" << std::endl;
+}
+
+void SimpleGraphicsLayer::Init(/* SGLFlags flags, */ int width, int height) {
+    sg_desc desc{};
+
+#if defined(WINDOW_SAPP)
+    swapchain = sglue_swapchain();
+    desc.environment = sglue_environment();
+    desc.logger.func = slog_func;
+#elif defined(WINDOW_SDL)
+    swapchain = {};
+    swapchain.width = width;
+    swapchain.height = height;
+    swapchain.color_format = SG_PIXELFORMAT_RGBA8;
+// TODO: uncomment this, ts *was* pmo
+// #else
+//     #error "No window backend defined! Define WINDOW_SAPP or WINDOW_SDL."
+#endif
+
+    sg_setup(&desc);
+
+    pass_action.colors[0].load_action = SG_LOADACTION_CLEAR;
+}
+
+void SimpleGraphicsLayer::Clear(SGLColour clear_col) {
+    pass_action.colors[0].clear_value = clear_col;
+}
+
+void SimpleGraphicsLayer::Render() {
+    sg_begin_pass((sg_pass){
+        .action = pass_action,
+        .swapchain = swapchain
+    });
+    sg_end_pass();
+    sg_commit();
 }
