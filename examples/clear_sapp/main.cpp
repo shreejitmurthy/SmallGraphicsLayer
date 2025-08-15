119c5b05049001fdc764c09d5a3e6b8645b692da
Shreejit Murthy <botmorph17@gmail.com>
2025-08-16 01:55:10 +1000

    sapp clear example

---
+++ examples/clear_sapp/main.cpp
@@ -0,0 +1,34 @@
+#include "sokol_app.h"
+#include "sokol_log.h"
+
+#include "SGL/SmallGraphicsLayer.hpp"
+
+sg_pass_action pass_action;
+
+SimpleGraphicsLayer sgl;
+
+void init(void) {
+    sgl.Init();
+}
+
+void frame(void) {
+    sgl.Clear();
+    sgl.Render();
+}
+
+void cleanup(void) {
+    sg_shutdown();
+}
+
+int main(int argc, char** argv) {
+    sapp_run((sapp_desc){
+        .init_cb = init,
+        .frame_cb = frame,
+        .cleanup_cb = cleanup,
+        .width = 800,
+        .height = 600,
+        .window_title = "Window",
+        .icon.sokol_default = true,
+        .logger.func = slog_func,
+    });
+}
