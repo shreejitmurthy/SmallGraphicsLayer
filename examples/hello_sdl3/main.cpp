53216dcc746e1eca75823d3db3475567408b19cd
Shreejit Murthy <botmorph17@gmail.com>
2025-08-16 00:13:02 +1000

    sdl3 open window

--- examples/hello_sdl3/main.cpp
+++ examples/hello_sdl3/main.cpp
@@ -1,8 +1,51 @@
-// examples/hello_sdl3/main.cpp
-
 #include "SGL/SmallGraphicsLayer.hpp"
 
-int main(int argc, char** argv) {
-    HelloSGL();
+#include <SDL3/SDL.h>
+#include <SDL3/SDL_main.h>
+
+int main() {
+    if (!SDL_Init(SDL_INIT_VIDEO)) {
+    }
+
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
+    const int screenWidth = 800;
+    const int screenHeight = 600;
+
+    SDL_Window* window = SDL_CreateWindow("Window", 800, 600, SDL_WINDOW_OPENGL);
+    SDL_GLContext ctx = SDL_GL_CreateContext(window);
+    bool open = true;
+
+    SDL_GL_SetSwapInterval(1);
+
+    SDL_Event event;
+
+    while (open) {
+        while (SDL_PollEvent(&event)) {
+            switch (event.type) {
+                case SDL_EVENT_QUIT:
+                    open = false;
+                    break;
+
+                case SDL_EVENT_KEY_DOWN:
+                    if (event.key.key == SDLK_ESCAPE) {
+                        open = false;
+                    }
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        SDL_GL_SwapWindow(window);
+    }
+
+    SDL_DestroyWindow(window);
+    SDL_GL_DestroyContext(ctx);
+    SDL_Quit();
+
     return 0;
 }
