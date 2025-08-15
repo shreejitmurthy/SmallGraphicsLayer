f9b15ba4cee72934ad95c744bf1a9c71c38ec545
Shreejit Murthy <botmorph17@gmail.com>
2025-08-16 03:34:31 +1000

    move

---
+++ examples/sokol.mm
@@ -0,0 +1,26 @@
+// Required for macOS.
+
+#if defined(__APPLE__) && defined(__MACH__)
+    #define SOKOL_IMPL
+    #define SOKOL_GLCORE
+    #define SOKOL_NO_ENTRY
+    #define SOKOL_TRACE_HOOKS
+    #include "sokol/sokol_app.h"
+    #include "sokol/sokol_gfx.h"
+    #include "sokol/sokol_time.h"
+    // #include "sokol_audio.h"
+    #include "sokol/sokol_fetch.h"
+    #include "sokol/sokol_log.h"
+    #include "sokol/sokol_glue.h"
+    #if defined(_MSC_VER )
+        // #pragma warning(disable:4996)   // strncpy use in fontstash.h
+    #endif
+    #if defined(__GNUC__) || defined(__clang__)
+        #pragma GCC diagnostic push
+        #pragma GCC diagnostic ignored "-Wunused-function"
+        #pragma GCC diagnostic ignored "-Wsign-conversion"
+    #endif
+    #if defined(__GNUC__) || defined(__clang__)
+        #pragma GCC diagnostic pop
+    #endif
+#endif
