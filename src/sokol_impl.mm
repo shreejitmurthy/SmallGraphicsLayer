c30282481984aaace497aa349381bdc2e9ee4c22
Shreejit Murthy <botmorph17@gmail.com>
2025-08-16 01:55:24 +1000

    sokol build anchors

---
+++ src/sokol_impl.mm
@@ -0,0 +1,10 @@
+#define SOKOL_APP_IMPL
+#define SOKOL_GFX_IMPL
+#define SOKOL_GLUE_IMPL
+#define SOKOL_LOG_IMPL
+#define SOKOL_GLCORE
+
+#include "sokol/sokol_app.h"
+#include "sokol/sokol_gfx.h"
+#include "sokol/sokol_glue.h"
+#include "sokol/sokol_log.h"
