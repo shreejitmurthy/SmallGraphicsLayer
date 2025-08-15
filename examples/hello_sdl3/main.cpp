503986f2054da028ea39e5c7b521d741d4221924
Shreejit Murthy <botmorph17@gmail.com>
2025-08-16 00:09:13 +1000

    initial commit

---
+++ examples/hello_sdl3/main.cpp
@@ -0,0 +1,8 @@
+// examples/hello_sdl3/main.cpp
+
+#include "SGL/SmallGraphicsLayer.hpp"
+
+int main(int argc, char** argv) {
+    HelloSGL();
+    return 0;
+}
---
+++ src/SmallGraphicsLayer.cpp
@@ -0,0 +1,7 @@
+#include "SGL/SmallGraphicsLayer.hpp"
+
+#include <iostream>
+
+void HelloSGL() {
+    std::cout << "Hello, SGL!" << std::endl;
+}
