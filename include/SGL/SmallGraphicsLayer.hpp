d1622f5b5ea533d9dce9ae635c106dde1db378a4
Shreejit Murthy <botmorph17@gmail.com>
2025-08-17 03:16:33 +1000

    create AttributeBuilder class

--- include/SGL/SmallGraphicsLayer.hpp
+++ include/SGL/SmallGraphicsLayer.hpp
@@ -2,19 +2,57 @@
 
 #include <sokol/sokol_gfx.h>
 
-#include <cstdint>
+#include "genshaders/attributes.glsl.h"
 
-typedef sg_color SGLColour;
+#include <cstdint>
+#include <vector>
+#include <iostream>
 
 namespace SmallGraphicsLayer {
-    class Device {
-    public:
-        void Init(/* SGLFlags flags, */ int width = 0, int height = 0);
-        void Clear(SGLColour clear_col = {0.15f, 0.15f, 0.2f, 1.0f});
-        void Render();
-        void Shutdown();
-    private:
-        sg_pass_action pass_action;
-        sg_swapchain swapchain;
-    };
+
+typedef sg_color Colour;
+
+struct Colours {
+    static constexpr Colour White = {1, 1, 1, 1};
+    static constexpr Colour Red   = {1, 0, 0, 1};
+    static constexpr Colour Green = {0, 1, 0, 1};
+    static constexpr Colour Blue  = {0, 0, 1, 1};
+};
+
+
+class Device {
+public:
+    void Init(/* SGLFlags flags, */ int width = 0, int height = 0);
+    void Clear(Colour clear_col = {0.15f, 0.15f, 0.2f, 1.0f});
+    void Refresh();
+    void Shutdown();
+private:
+    sg_pass_action pass_action;
+    sg_swapchain swapchain;
+};
+
+struct Position { float x, y, z; };
+
+enum class Primitives {
+    Triangle = 3
+};
+
+class AttributeBuilder {
+public:
+    AttributeBuilder& Begin(Primitives primative);
+    AttributeBuilder& Vertex(Position pos, Colour col);
+    void End();
+    void Draw();
+    void Destroy();
+
+private:
+    int elements;
+    std::vector<float> vertices;
+
+    sg_shader shd = {};
+    sg_buffer vbuf = {};
+    sg_buffer ibuf = {};
+    sg_bindings bindings = {};
+    sg_pipeline pipeline = {};
+};
 };
--- src/SmallGraphicsLayer.cpp
+++ src/SmallGraphicsLayer.cpp
@@ -6,6 +6,8 @@
     #include <sokol/sokol_log.h>
 #endif
 
+#include <array>
+
 #include <sokol/sokol_gfx.h>
 
 void SmallGraphicsLayer::Device::Init(int width, int height) {
@@ -30,19 +32,69 @@ void SmallGraphicsLayer::Device::Init(int width, int height) {
         pass_action.colors[0].load_action = SG_LOADACTION_CLEAR;
 }
 
-void SmallGraphicsLayer::Device::Clear(SGLColour clear_col) {
+void SmallGraphicsLayer::Device::Clear(Colour clear_col) {
     pass_action.colors[0].clear_value = clear_col;
-}
 
-void SmallGraphicsLayer::Device::Render() {
     sg_begin_pass((sg_pass){
         .action    = pass_action,
         .swapchain = swapchain
     });
+}
+
+void SmallGraphicsLayer::Device::Refresh() {
     sg_end_pass();
     sg_commit();
 }
 
 void SmallGraphicsLayer::Device::Shutdown() {
     sg_shutdown();
+}
+
+SmallGraphicsLayer::AttributeBuilder& SmallGraphicsLayer::AttributeBuilder::Begin(Primitives primative) {
+    elements = static_cast<int>(primative);
+
+    shd = sg_make_shader(attributes_main_shader_desc(sg_query_backend()));
+
+    // not cross-platform safe
+    pipeline = sg_make_pipeline((sg_pipeline_desc){
+        .shader = shd,
+        .layout = {
+            .attrs = {
+                [ATTR_attributes_main_position].format = SG_VERTEXFORMAT_FLOAT3,
+                [ATTR_attributes_main_aColor].format = SG_VERTEXFORMAT_FLOAT4
+            }
+        },
+    });
+
+    return *this;
+}
+
+// perhaps a bool that asks for pixel to NDC for help? default to true.
+SmallGraphicsLayer::AttributeBuilder& SmallGraphicsLayer::AttributeBuilder::Vertex(Position pos, Colour col) {
+    std::array<float, 7> chunk = {pos.x, pos.y, pos.z,  col.r, col.g, col.b, col.a};
+    vertices.insert(vertices.end(), chunk.begin(), chunk.end());
+    // should personally throw an error if too many chunks are inserted
+    // should personally throw a warn if there are less than expected as well
+    return *this;
+}
+
+void SmallGraphicsLayer::AttributeBuilder::End() {
+    // not cross-platform safe
+    bindings.vertex_buffers[0] = sg_make_buffer((sg_buffer_desc){
+        .size = vertices.size() * sizeof(float),
+        .data = {.ptr = vertices.data(), .size = vertices.size() * sizeof(float)},
+    });
+}
+
+void SmallGraphicsLayer::AttributeBuilder::Draw() {
+    sg_apply_pipeline(pipeline);
+    sg_apply_bindings(&bindings);
+    sg_draw(0, elements, 1);
+}
+
+void SmallGraphicsLayer::AttributeBuilder::Destroy() {
+    sg_destroy_shader(shd);
+    sg_destroy_buffer(vbuf);
+    sg_destroy_buffer(ibuf);
+    sg_destroy_pipeline(pipeline);
 }
