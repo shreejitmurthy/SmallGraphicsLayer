cb2d3aab126b460a53d2f174ac9e2f9ad9a2f731
Shreejit Murthy <botmorph17@gmail.com>
2025-08-16 17:41:27 +1000

    switch convention

--- include/SGL/SmallGraphicsLayer.hpp
+++ include/SGL/SmallGraphicsLayer.hpp
@@ -6,12 +6,13 @@
 
 typedef sg_color SGLColour;
 
-class SimpleGraphicsLayer {
-public:
+struct {
+    sg_pass_action pass_action;
+    sg_swapchain swapchain;
+} SGLState;
+
+namespace SmallGraphicsLayer {
     void Init(/* SGLFlags flags, */ int width = 0, int height = 0);
     void Clear(SGLColour clear_col = {0.15f, 0.15f, 0.2f, 1.0f});
     void Render();
-private:
-    sg_pass_action pass_action;
-    sg_swapchain swapchain;
 };
--- src/SmallGraphicsLayer.cpp
+++ src/SmallGraphicsLayer.cpp
@@ -10,42 +10,36 @@
 
 #include <sokol/sokol_gfx.h>
 
-#include "SGL/SmallGraphicsLayer.hpp"
-
-void HelloSGL() {
-    std::cout << "Hello, SGL!" << std::endl;
-}
-
-void SimpleGraphicsLayer::Init(/* SGLFlags flags, */ int width, int height) {
+void SmallGraphicsLayer::Init(int width, int height) {
     sg_desc desc{};
 
-#if defined(WINDOW_SAPP)
-    swapchain = sglue_swapchain();
-    desc.environment = sglue_environment();
-    desc.logger.func = slog_func;
-#elif defined(WINDOW_SDL)
-    swapchain = {};
-    swapchain.width = width;
-    swapchain.height = height;
-    swapchain.color_format = SG_PIXELFORMAT_RGBA8;
-// TODO: uncomment this, ts *was* pmo
-// #else
-//     #error "No window backend defined! Define WINDOW_SAPP or WINDOW_SDL."
-#endif
+    #if defined(WINDOW_SAPP)
+        SGLState.swapchain = sglue_swapchain();
+        desc.environment = sglue_environment();
+        desc.logger.func = slog_func;
+    #elif defined(WINDOW_SDL)
+        SGLState.swapchain = {};
+        SGLState.swapchain.width = width;
+        SGLState.swapchain.height = height;
+        SGLState.swapchain.color_format = SG_PIXELFORMAT_RGBA8;
+    // TODO: uncomment this, ts *was* pmo
+    // #else
+    //     #error "No window backend defined! Define WINDOW_SAPP or WINDOW_SDL."
+    #endif
 
-    sg_setup(&desc);
+        sg_setup(&desc);
 
-    pass_action.colors[0].load_action = SG_LOADACTION_CLEAR;
+        SGLState.pass_action.colors[0].load_action = SG_LOADACTION_CLEAR;
 }
 
-void SimpleGraphicsLayer::Clear(SGLColour clear_col) {
-    pass_action.colors[0].clear_value = clear_col;
+void SmallGraphicsLayer::Clear(SGLColour clear_col) {
+    SGLState.pass_action.colors[0].clear_value = clear_col;
 }
 
-void SimpleGraphicsLayer::Render() {
+void SmallGraphicsLayer::Render() {
     sg_begin_pass((sg_pass){
-        .action = pass_action,
-        .swapchain = swapchain
+        .action    = SGLState.pass_action,
+        .swapchain = SGLState.swapchain
     });
     sg_end_pass();
     sg_commit();
