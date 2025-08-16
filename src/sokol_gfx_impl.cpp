e1851e25d1f45e523238008d1788958cdf7d21a5
Shreejit Murthy <botmorph17@gmail.com>
2025-08-16 18:04:28 +1000

    need for sdl3 as win backend

---
+++ src/sokol_gfx_impl.cpp
@@ -0,0 +1,8 @@
+// Required for SDL3 window backend (I'm pretty sure)
+
+#define SOKOL_GFX_IMPL
+#define SOKOL_GLCORE
+#include "sokol/sokol_gfx.h"
+
+#define SOKOL_LOG_IMPL
+#include "sokol/sokol_log.h"
