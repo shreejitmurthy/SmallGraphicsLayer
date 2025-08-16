514523d0cd74a90cc77dcfd61ae4ae131536377e
Shreejit Murthy <botmorph17@gmail.com>
2025-08-17 01:00:26 +1000

    cleanup

--- include/SGL/SmallGraphicsLayer.hpp
+++ include/SGL/SmallGraphicsLayer.hpp
@@ -3,31 +3,10 @@
 #include <sokol/sokol_gfx.h>
 
 #include <cstdint>
-#include <future>
 
 typedef sg_color SGLColour;
 
 namespace SmallGraphicsLayer {
-    enum class AssetType {
-        File,     // use normal c++ file i/o
-        Texture,  // use stb_image
-        Model,    // use assimp (or whatever i plan to use)
-        Sound     // use something idk
-    };
-
-    typedef struct File { std::string data; } File;
-
-    class AssetManager {
-    public:
-        void Request(const std::string& filepath, AssetType type = AssetType::File);
-        
-        template<typename T>
-        T* Get(const std::string& filepath);
-    private:
-        std::unordered_map<std::string, std::future<File>> files;
-        std::unordered_map<std::string, File> loadedFiles;
-    };
-
     class Device {
     public:
         void Init(/* SGLFlags flags, */ int width = 0, int height = 0);
--- include/SGL/Utils.hpp
+++ include/SGL/Utils.hpp
@@ -25,7 +25,6 @@ namespace SmallGraphicsLayer {
             }
         }
 
-    
         std::string LoadFileIntoString(const std::string& filepath) {
             namespace fs = std::filesystem;
 
