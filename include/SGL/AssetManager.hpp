80c095bb68afd1b0fefc6ee79c7f8c14eec9d616
Shreejit Murthy <botmorph17@gmail.com>
2025-08-17 02:46:25 +1000

    cleanup

--- include/SGL/AssetManager.hpp
+++ include/SGL/AssetManager.hpp
@@ -5,23 +5,23 @@
 #include <future>
 
 namespace SmallGraphicsLayer {
-    enum class AssetType {
-        File,     // use normal c++ file i/o
-        Texture,  // use stb_image
-        Model,    // use assimp (or whatever i plan to use)
-        Sound     // use something idk
-    };
+enum class AssetType {
+    File,     // use normal c++ file i/o
+    Texture,  // use stb_image
+    Model,    // use assimp (or whatever i plan to use)
+    Sound     // use something idk
+};
 
-    typedef struct File { std::string data; } File;
+typedef struct File { std::string data; } File;
 
-    class AssetManager {
-    public:
-        void Request(const std::string& filepath, AssetType type = AssetType::File);
-        
-        template<typename T>
-        T* Get(const std::string& filepath);
-    private:
-        std::unordered_map<std::string, std::future<File>> files;
-        std::unordered_map<std::string, File> loadedFiles;
-    };
+class AssetManager {
+public:
+    void Request(const std::string& filepath, AssetType type = AssetType::File);
+    
+    template<typename T>
+    T* Get(const std::string& filepath);
+private:
+    std::unordered_map<std::string, std::future<File>> files;
+    std::unordered_map<std::string, File> loadedFiles;
+};
 }
--- include/SGL/Utils.hpp
+++ include/SGL/Utils.hpp
@@ -10,38 +10,37 @@
 namespace fs = std::filesystem;
 
 namespace SmallGraphicsLayer {
-    namespace Utils {
-        fs::path FindPathUpwards(const fs::path& targetPath, fs::path startDir) {
-            for (;;) {
-                const fs::path candidate = startDir / targetPath;
-                if (fs::exists(candidate)) {
-                    return fs::absolute(candidate);
-                }
-                if (startDir == startDir.root_path()) {
-                    std::cout << "Target " << targetPath << " not found.\n";
-                    return {};
-                }
-                startDir = startDir.parent_path();
-            }
+namespace Utils {
+fs::path FindPathUpwards(const fs::path& targetPath, fs::path startDir) {
+    for (;;) {
+        const fs::path candidate = startDir / targetPath;
+        if (fs::exists(candidate)) {
+            return fs::absolute(candidate);
         }
-
-        std::string LoadFileIntoString(const std::string& filepath) {
-            namespace fs = std::filesystem;
-
-            fs::path startDir = fs::current_path();
-            fs::path foundPath = FindPathUpwards(filepath, startDir);
-
-            if (foundPath.empty()) {
-                return std::string("[LoadFile] Failed to locate: ") + filepath;
-            }
-            std::ifstream in(foundPath, std::ios::in | std::ios::binary);
-            if (!in) {
-                return std::string("[LoadFile] Failed to open: ") + foundPath.string();
-            }
-            std::ostringstream ss;
-            ss << in.rdbuf();
-            return ss.str();
+        if (startDir == startDir.root_path()) {
+            std::cout << "Target " << targetPath << " not found.\n";
+            return {};
         }
-
+        startDir = startDir.parent_path();
     }
+}
+
+std::string LoadFileIntoString(const std::string& filepath) {
+    namespace fs = std::filesystem;
+
+    fs::path startDir = fs::current_path();
+    fs::path foundPath = FindPathUpwards(filepath, startDir);
+
+    if (foundPath.empty()) {
+        return std::string("[LoadFile] Failed to locate: ") + filepath;
+    }
+    std::ifstream in(foundPath, std::ios::in | std::ios::binary);
+    if (!in) {
+        return std::string("[LoadFile] Failed to open: ") + foundPath.string();
+    }
+    std::ostringstream ss;
+    ss << in.rdbuf();
+    return ss.str();
+}
+}
 };
