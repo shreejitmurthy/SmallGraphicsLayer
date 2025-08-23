38952f75b45fd480240b60ce41d03f15cb0147f4
Shreejit Murthy <botmorph17@gmail.com>
2025-08-23 14:01:43 +1000

    shader and generated file

---
+++ shaders/ssprite.glsl
@@ -0,0 +1,30 @@
+@ctype mat4 SmallGraphicsLayer::Math::Mat4
+
+@vs vs
+layout(binding=0) uniform params {
+    mat4 mvp;
+};
+
+in vec4 pos;
+in vec2 texcoord0;
+out vec2 uv;
+
+void main() {
+    gl_Position = mvp * pos;
+    uv = texcoord0;
+}
+@end
+
+@fs fs
+layout(binding=0) uniform texture2D tex;
+layout(binding=0) uniform sampler smp;
+
+in vec2 uv;
+out vec4 frag_color;
+
+void main() {
+    frag_color = texture(sampler2D(tex, smp), uv);
+}
+@end
+
+@program main vs fs
