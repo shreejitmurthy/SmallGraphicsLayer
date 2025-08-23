bb82784d590b2af80d864057b6729b90a9d7f23b
Shreejit Murthy <botmorph17@gmail.com>
2025-08-23 14:22:51 +1000

    name consistency

--- include/SGL/Utils.hpp
+++ include/SGL/Utils.hpp
@@ -12,7 +12,7 @@ namespace fs = std::filesystem;
 #include "Math.hpp"
 
 namespace SmallGraphicsLayer::Utils {
-fs::path FindPathUpwards(const fs::path& targetPath, fs::path startDir) {
+fs::path FindPathUpwards(const fs::path& targetPath, fs::path startDir = fs::current_path()) {
     for (;;) {
         const fs::path candidate = startDir / targetPath;
         if (fs::exists(candidate)) {
@@ -29,8 +29,7 @@ fs::path FindPathUpwards(const fs::path& targetPath, fs::path startDir) {
 std::string LoadFileIntoString(const std::string& filepath) {
     namespace fs = std::filesystem;
 
-    fs::path startDir = fs::current_path();
-    fs::path foundPath = FindPathUpwards(filepath, startDir);
+    fs::path foundPath = FindPathUpwards(filepath);
 
     if (foundPath.empty()) {
         return std::string("[LoadFile] Failed to locate: ") + filepath;
